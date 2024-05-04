6c5c5eab86b328efff6db3c9cc66c340c238f34d
GUR <gura.anton@gmail.com>
2024-05-04 08:45:15 +0300

    HW2.3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,13 @@
+// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає 
+// результат перевірки.Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+
+// Доповни код функції таким чином, що:
+
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+
 function checkForSpam(message) {
-    if (message.toLowerCase().includes('spam', 'sale')) {
+    if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')) {
         return true;
     } else {
         return false;
---
+++ js/test2.js
@@ -0,0 +1,26 @@
+// Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
+
+function check(value) {
+    if (value > 0) {
+        console.log (`${value} - число додаттнє`)
+    } else {
+        console.log (`${value} - число від'ємне`)
+    }
+}
+check(5)
+check(-5)
+console.log("//////TASK2//////////")
+
+// Дана строка. Выведите в консоль длину этой строки.
+
+let str = 'fwefwefwfnpjng;vkjnfvk;'
+console.log(str.length);
+
+console.log("//////TASK3//////////")
+
+// Дана строка. Выведите в консоль последний символ строки.
+
+let str1 = 'fwefwefwfnpjng;vkjnfvkP'
+console.log(str1)
+let lastChar = str1.charAt(srt1.length-1)
+console.log(lastChar);
